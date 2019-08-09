3beb437910f238e3fad826e0422a03f817b913e2
Edouard De Prez <deprezedouard@gmail.com>
2019-08-09 11:25:22 +0200

    seed updated

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,7 +8,6 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = Ingredient.find(params[:dose][:ingredient_id])
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -29,6 +28,6 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ require 'json'
 require 'open-uri'
 
 puts 'Cleaning database...'
+Dose.destroy_all
+Cocktail.destroy_all
 Ingredient.destroy_all
 
 puts 'Creating ingredients...'
@@ -9,9 +11,24 @@ puts 'Creating ingredients...'
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
+ings = []
 
 ingredients['drinks'].each do |ingredient|
-  Ingredient.create(name: ingredient['strIngredient1'])
+  ings << Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+puts 'Creating cocktails...'
+
+cocktails = ['Moscow Mule', 'Pina Colada', 'Gin Tonic', 'Vodka Cola']
+cocktails.each do |cocktail|
+  coc = Cocktail.create(name: cocktail)
+  4.times do
+    dose = Dose.new(description: "#{Random.new.rand(1..100)}cl")
+    dose.cocktail = coc
+    dose.ingredient = ings.sample
+    dose.save
+  end
+  coc.save
 end
 
 puts 'Finished!'
